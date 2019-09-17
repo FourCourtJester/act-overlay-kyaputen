ce16625cf5f3de4784ae9dc893dce1e7ab76b0e5
FourCourtJester3PT <fourcourtjester@3pt-ops.com>
2019-09-16 21:05:45 -0400

    - Added github relative URL replacement

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,13 @@ const
 router.get(['/', '/job/:job'], function (req, res, next) {
     // Render the page
     res.render('index', (err, html) => {
+        const dev_html = html
         // Disable caching
         res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate') // HTTP 1.1
         res.setHeader('Pragma', 'no-cache') // HTTP 1.0
         res.setHeader('Expires', '0') // Proxies
+
+        html = html.replace('/public', '/act-overlay-kyaputen/public')
 
         // Public accessible page
         fs.writeFileSync('index.html', html)
@@ -20,7 +23,7 @@ router.get(['/', '/job/:job'], function (req, res, next) {
         jobs.forEach((job) => fs.copySync('index.html', `job/${job}/index.html`))
 
         // Development page
-        res.send(html)
+        res.send(dev_html)
     })
 })
 
